969c8985211327369838be403ff9c4e7081f3f1f
Rakshitha-Vani <rakshithavani976@gmail.com>
2026-04-18 11:49:44 +0530

    React App

---
+++ todo-app/src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function TodoItem({todo, index, deleteTodo}) {
+    return (
+        <div>
+            <p>{todo}</p>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+        </div>
+    )
+}
+
+export default TodoItem
--- todo-app/src/components/Todoinput.jsx
+++ todo-app/src/components/Todoinput.jsx
@@ -1,10 +1,11 @@
-import {userState } from "react"
-function Todoinput(addTodo) {
+import { useState } from "react"
+
+function Todoinput({ addTodo }) {
     const [todo, setTodo] = useState("")
 
     const handleChange = () => {
         if (todo.trim() !== "") {
-            addTodo(input)
+            addTodo(todo)
             setTodo("")
         }
     }
